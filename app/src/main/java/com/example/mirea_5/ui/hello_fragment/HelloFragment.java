86e600cad27c58d567cefb2e17f8d85e2128d25c
TamziF <AndrSh01@yandex.ru>
2024-03-23 16:34:42 +0300

    6 initial commit

--- app/src/main/java/com/example/mirea_5/ui/hello_fragment/HelloFragment.java
+++ app/src/main/java/com/example/mirea_5/ui/hello_fragment/HelloFragment.java
@@ -2,6 +2,7 @@ package com.example.mirea_5.ui.hello_fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -9,12 +10,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mirea_5.NotificationManager;
 import com.example.mirea_5.R;
 import com.example.mirea_5.databinding.FragmentHelloBinding;
 
 public class HelloFragment extends Fragment {
 
     private FragmentHelloBinding binding;
+
+    private NotificationManager notificationManager;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        notificationManager = new NotificationManager(requireContext());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +36,20 @@ public class HelloFragment extends Fragment {
                 Navigation.findNavController(v).navigate(R.id.action_helloFragment_to_notes)
         );
 
+        binding.notificationButton.setOnClickListener(v -> {
+            requireActivity().requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 1);
+            showNotification();
+        });
+
         return binding.getRoot();
+    }
+
+    private void showNotification() {
+        String title = "Hello";
+        String text = "Please, don't forget to write new notes";
+        int drawable = R.drawable.ic_launcher_foreground;
+
+        notificationManager.sendNotification(title, text, drawable, requireContext());
     }
 
 }
